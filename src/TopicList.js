df3f14f3fc4d7c55cedcf6f82d92ae5553c23fe5
William <william.berthouex@gmail.com>
2024-09-21 03:10:11 +0000

    Lecture3 Part2

--- src/TopicList.js
+++ src/TopicList.js
@@ -1,4 +1,5 @@
 import Accordion from 'react-bootstrap/Accordion';
+import TopicThread from './TopicThread';
 
 function TopicList() {
     return (
@@ -6,26 +7,14 @@ function TopicList() {
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Accordion Item #1</Accordion.Header>
                 <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                Test text line 1
                 </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="1">
                 <Accordion.Header>Accordion Item #2</Accordion.Header>
                 <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                Test Text Line Capital
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
--- src/ViewPort.js
+++ src/ViewPort.js
@@ -19,5 +19,5 @@ function ViewPort() {
       </Container>
     );
 }
-  
+
 export default ViewPort;
